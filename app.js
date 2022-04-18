5e5cc44ebd044e1a8ee624b9e42e85a5dcbdd0c5
PrinceJain19 <jainprince19112000@gmail.com>
2022-04-18 15:55:19 +0530

    fixing soft keyboard issue on android

--- app.js
+++ app.js
@@ -43,3 +43,7 @@ function songPause() {
 btnTranslate.addEventListener("click", clickHandler)
 btnPlayMusic.addEventListener("click", songPlay)
 btnPauseMusic.addEventListener("click", songPause)
+
+setTimeout(()=>
+   document.querySelector("meta[name=viewport]").setAttribute("content", "height=" + screen.height*0.95 + "px, width=device-width, initial-scale=1.0")
+, 300);
